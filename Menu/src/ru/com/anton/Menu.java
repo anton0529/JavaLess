f2c06a10f3e4a3cca4827e3df1c0256288bf6ddb
Anton <anton123098@gmail.com>
2021-04-23 21:18:45 +0300

    13

--- Menu/src/ru/com/anton/Menu.java
+++ Menu/src/ru/com/anton/Menu.java
@@ -1,57 +1,7 @@
 package ru.com.anton;
 
 import java.io.IOException;
-<<<<<<< HEAD
 import java.util.Scanner;
-
-public class Menu {
-
-	public static void main(String[] args){
-			System.out.println("Справка по оператору:");
-			System.out.println("     1. IF");
-			System.out.println("     2. SWITCH");
-			System.out.println("     3. WHILE");
-			System.out.println("     4. DO-WHILE");
-			System.out.println("     5. FOR \n");
-			System.out.println("Выберите нужный пункт");
-			System.out.println("\n");
-			Scanner scanner = new Scanner(System.in);
-			int choice = scanner.nextInt();
-		
-		switch(choice) {
-				case 1:
-					System.out.println("if: \n");
-					System.out.println("If(Условие) оператор;");
-					System.out.println("else оператор;");
-					break;
-				case 2:
-					System.out.println("switch \n");
-					System.out.println("switch(выражение) { ");
-					System.out.println(" case константа :");
-					System.out.println(" последовательность операторов");
-					System.out.println(" break;");
-					System.out.println("// ... ");
-					System.out.println("}");
-					break;
-				case 3:
-					System.out.println("while \n");
-					System.out.println("while (условие) оператор;");
-					break;
-				case 4:
-					System.out.println("do-while \n");
-					System.out.println("do {");
-					System.out.println(" оператор");
-					System.out.println("} while (условие)");
-					break;
-				case 5:
-					System.out.println("for \n");
-					System.out.println("for (инициализация; условие; итерация;");
-					System.out.println(" оператор");
-					break;
-			default:
-				System.out.println(choice);
-		}
-=======
 
 public class Menu {
 
@@ -65,7 +15,8 @@ public class Menu {
 			System.out.println("     4. DO-WHILE");
 			System.out.println("     5. FOR \n");
 			System.out.println("Выберите нужный пункт");
-			choice = (int) System.in.read();
+			Scanner scanner = new Scanner(System.in);
+			choice = scanner.nextInt();
 		} while (choice < 1 || choice > 5);
 		
 		System.out.println("\n");
@@ -100,7 +51,6 @@ public class Menu {
 				System.out.println(" оператор");
 				break;
 			}
->>>>>>> main
 	}
 
 }
